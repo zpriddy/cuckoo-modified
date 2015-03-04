b65eecb3dccd7711ab5c6d6689cb464d8a4e3451
KillerInstinct <hodges92@hotmail.com>
2015-03-03 19:42:14 -0500

    Add the 'open' button to the human module. Also fix a bug where the human module would click Don't Run instead of Run when presented with both of these buttons. This also introduces the ability to blacklist button text. With this commit we can now analyze newer malspam campaigns which are spreading via .js attachments. The ie package runs these files to execute the payload.

--- analyzer/windows/modules/auxiliary/human.py
+++ analyzer/windows/modules/auxiliary/human.py
@@ -36,6 +36,11 @@ def foreach_child(hwnd, lparam):
         "don't send",
         "continue",
         "unzip",
+        "open",
+    ]
+
+    dontclick = [
+        "don't run",
     ]
 
     classname = create_unicode_buffer(50)
@@ -51,6 +56,9 @@ def foreach_child(hwnd, lparam):
         # Check if the button is "positive".
         for button in buttons:
             if button in text.value.lower():
+                for nots in dontclick:
+                    if nots in text.value.lower():
+                        return False
                 log.info("Found button \"%s\", clicking it" % text.value)
                 USER32.SetForegroundWindow(hwnd)
                 KERNEL32.Sleep(1000)
